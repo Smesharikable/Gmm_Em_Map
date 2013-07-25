0e39b85c87500a5617c52458c5d0987ea195b80b
1 <1@192.168.1.123>
2013-07-25 12:56:03 +0400

    Map.fitByWeights algorithm corrected

--- src/algorithms/Map.java
+++ src/algorithms/Map.java
@@ -60,51 +60,50 @@ public class Map {
         return OutModel;
     }
     
-    public GMM[] fitByWeights(Matrix input, int iterations, int states) {
-        GMM[] result = new GMM[states];
-        int chunk = input.getColumnDimension() / states;
-        int low = 0;
-        
-        for (int i = 0; i < states - 1 ; i++) {
-            result[i] = fitByWeights(
-                    input.getMatrix(0, input.getRowDimension() - 1, low, low + chunk - 1), 
-                    iterations
-                    );
-            low += chunk;
-        }
-        
-        result[states - 1] = fitByWeights(
-                    input.getMatrix(0, input.getRowDimension() - 1, low, input.getColumnDimension() - 1), 
-                    iterations
-                    );
-        
-        return result;
-    }
+//    public GMM[] fitByWeights(Matrix input, int iterations, int states) {
+//        GMM[] result = new GMM[states];
+//        int chunk = input.getColumnDimension() / states;
+//        int low = 0;
+//        
+//        for (int i = 0; i < states - 1 ; i++) {
+//            result[i] = fitByWeights(
+//                    input.getMatrix(0, input.getRowDimension() - 1, low, low + chunk - 1), 
+//                    iterations
+//                    );
+//            low += chunk;
+//        }
+//        
+//        result[states - 1] = fitByWeights(
+//                    input.getMatrix(0, input.getRowDimension() - 1, low, input.getColumnDimension() - 1), 
+//                    iterations
+//                    );
+//        
+//        return result;
+//    }
     
-    private GMM fitByWeights(Matrix input, int iterations) {
-        Matrix[] oldMu = mInModel.getMu();
-        Matrix[] oldSigma = mInModel.getSigma();
-        double[] P = mInModel.getP();
-        int count = mInModel.getNComponents();
-        GMM OutModel = mInModel;
+    /**
+     *
+     * @param input
+     * @param iterations
+     * @return the new GMM that fitted by weights on input vectors
+     */
+    public GMM fitByWeights(Matrix input, int iterations) {
+        GMM OutModel = mInModel.copy();
+        double[] newP = OutModel.getP();
+        int count = OutModel.getNComponents();
         int vectorCount = input.getColumnDimension();
-        double[] oldP = new double[count];
-        double[] newP = new double[count];
         
         double midWeight;
         double coeff;
-        System.arraycopy(P, 0, oldP, 0, count);
         
         for (int t = 0; t < iterations; t ++) {
             Matrix[] posterior = OutModel.posterior(input);
             for (int i = 0; i < count; i ++) {
                 midWeight = GMM.posteriorSum(posterior[i]);
                 coeff = midWeight / (midWeight + mRelativeCoeff);
-                newP[i] = midWeight * coeff / vectorCount + oldP[i] * (1 - coeff);
+                newP[i] = midWeight * coeff / vectorCount + newP[i] * (1 - coeff);
             }
             newP = GMM.normilize(newP);
-            oldP = Arrays.copyOf(oldP, count);
-            OutModel = new GMM(oldMu, oldSigma, newP);
         }
         
         
